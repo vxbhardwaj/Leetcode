a50c97596c1e68c4b30494b0dcba0fe75c1f6b7c
vxbhardwaj <76259177+vxbhardwaj@users.noreply.github.com>
2022-06-04 17:28:30 -0400

    lc 733

--- 733 Flood Fill.py
+++ 733 Flood Fill.py
@@ -1,14 +1,13 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        row,column, original=len(image),len(image[0]),image[sr][sc]
-        def dfs(i,j):
-            if i<0 or j<0 or i>=row or j>=column or image[i][j]==newColor or image[i][j]!=original:
+        row,column, img= len(image),len(image[0]),image[sr][sc]
+        def bfs(i,j):
+            if i<0 or j<0 or i>=row or j>=column or image[i][j]==newColor or img!=image[i][j]:
                 return
             image[i][j]=newColor
-            dfs(i-1,j)
-            dfs(i+1,j)
-            dfs(i,j-1)
-            dfs(i,j+1)
-        dfs(sr,sc)
+            bfs(i,j-1)
+            bfs(i,j+1)
+            bfs(i-1,j)
+            bfs(i+1,j)
+        bfs(sr,sc)
         return image
-            
